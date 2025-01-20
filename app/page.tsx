7408c789a9e58b4fccc532f82824f4183f8d163b
Aleksei <alexey.frolov174@gmail.com>
2025-01-20 01:38:36 +0100

    Initial project version

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import Banner from "@/components/banner.client";
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-14 ">
+      <Banner />
+    </main>
+  );
+}
